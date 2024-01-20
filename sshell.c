efe2fecbb13445e587dd2e25f14aba7017054654
MrGrinchFx <2003kevinle@gmail.com>
2024-01-20 13:43:42 -0800

    pwd and cd

--- sshell.c
+++ sshell.c
@@ -14,35 +14,38 @@ char **whitespace_delimiter(char *cmd)
     while (argument != NULL)
     {
         args = realloc(args, count * 32);
-        args[count-2] = argument;
+        args[count - 2] = argument;
         argument = strtok(NULL, " ");
         count++;
     }
     args = realloc(args, count * 32);
-    args[count+1] = NULL;
+    args[count + 1] = NULL;
     return args;
 }
 
 int custom_system(char *cmd)
 {
-    char** args = whitespace_delimiter(cmd);
+    char **args = whitespace_delimiter(cmd);
     pid_t pid;
     pid = fork();
 
     if (pid == 0)
     {
+        /*Child Process*/
         execvp(args[0], args);
         perror("execv");
         exit(1);
     }
     else if (pid > 0)
     {
+        /*Parent Process*/
         int status;
         waitpid(pid, &status, 0);
-        //printf("Child returned %d\n", WEXITSTATUS(status));
+        // printf("Child returned %d\n", WEXITSTATUS(status));
     }
     else
     {
+        // Error Returned
         perror("fork");
         exit(1);
     }
@@ -59,7 +62,7 @@ int main(void)
         char *nl;
         int retval;
 
-        /* Print prompt poopy */
+        /* Print prompt */
         printf("sshell@ucd$ ");
         fflush(stdout);
 
@@ -78,14 +81,38 @@ int main(void)
         if (nl)
             *nl = '\0';
 
-        /* Builtin command */
+        /* Builtin commands */
         if (!strcmp(cmd, "exit"))
         {
             fprintf(stderr, "Bye...\n");
             break;
         }
+        char **args = whitespace_delimiter(cmd);
+        /*CD COMMAND*/
+        if (!strcmp(args[0], "cd"))
+        {
+            int status = chdir(args[1]);
+            if (status != 0)
+            {
+                perror("cd");
+            }
+            continue;
+        }
+        /*PWD COMMAND*/
 
-        /* Regular command change this so its doesn't use system*/
+        if (!strcmp(args[0], "pwd"))
+        {
+            char cwd[1024];
+            char *status = getcwd(cwd, sizeof(cwd));
+            if (status == NULL)
+            {
+                perror("pwd");
+            }
+            printf("%s\n", cwd);
+            continue;
+        }
+
+        /* Regular command*/
         retval = custom_system(cmd);
         fprintf(stdout, "+ completed '%s' [%d]\n",
                 cmd, retval);

b413f575843dd051fa8985fcb401f971cb6ba532
MrGrinchFx <2003kevinle@gmail.com>
2024-01-21 23:13:15 -0800

    updated parser

--- sshell.c
+++ sshell.c
@@ -3,26 +3,110 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
 #include <fcntl.h>
 
 #define CMDLINE_MAX 512
 #define CWD_MAX 1024
-#define MAX_BYTES 32
+#define MAX_ARG_SIZE 32
+#define MAX_NUM_ARGS 16
 
-char **whitespace_delimiter(char *cmd)
+// char **whitespace_delimiter(char *cmd)
+// {
+
+//     char *argument = strtok(cmd, " ");
+//     char **args = malloc(MAX_BYTES);
+//     int count = 2;
+//     while (argument != NULL)
+//     {
+//         args = realloc(args, count * MAX_BYTES);
+//         args[count - 2] = argument;
+//         argument = strtok(NULL, " ");
+//         count++;
+//     }
+//     args = realloc(args, count * MAX_BYTES);
+//     args[count + 1] = NULL;
+//     return args;
+// }
+
+/*TO DOOOOO: PLS MAKE SURE THAT WE CHECK IF BUFFER IS EMPTY WHEN WE ENCOUNTER THE KEY CHAR TO DETERMINE IF WE EXECUTE*/
+char **custom_parser(char *cmd)
 {
-    char *argument = strtok(cmd, " ");
-    char **args = malloc(MAX_BYTES);
-    int count = 2;
-    while (argument != NULL)
+    char buffer[MAX_ARG_SIZE + 1];
+    char **args = malloc(MAX_NUM_ARGS * sizeof(char *));
+    int pointer = 0;  // index of cmd
+    int count = 0;    // index of buffer
+    int argCount = 0; // number of arguments
+    while (cmd[pointer] != '\0')
     {
-        args = realloc(args, count * MAX_BYTES);
-        args[count - 2] = argument;
-        argument = strtok(NULL, " ");
-        count++;
+        if (count != 0 && cmd[pointer] == ' ')
+        { // check if buffer is empty
+            buffer[count] = '\0';
+            args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for buffer
+            strcpy(args[argCount], buffer);        // copy over buffer content
+            argCount++;
+            memset(buffer, '\0', sizeof(buffer)); // clear the buffer
+            count = 0;
+        }
+        else if (cmd[pointer] == '|')
+        {
+            if (count != 0)
+            { // check if buffer is empty
+                buffer[count] = '\0';
+                args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for buffer
+                strcpy(args[argCount], buffer);        // copy over content
+                argCount++;
+                memset(buffer, '\0', sizeof(buffer)); // clear buffer
+            }
+            args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for |
+            args[argCount] = "|";                  // add "|" to array
+            argCount++;
+            count = 0;
+        }
+        else if (cmd[pointer] == '>' && cmd[pointer + 1] == '>')
+        {
+            if (count != 0)
+            { // check if buffer is empty
+                buffer[count] = '\0';
+                args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for buffer
+                strcpy(args[argCount], buffer);        // copy over content
+                argCount++;
+                memset(buffer, '\0', sizeof(buffer)); // clear buffer
+            }
+            args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for >>
+            args[argCount] = ">>";                 // add ">" to array
+            argCount++;
+            count = 0; // set buffer back to 0
+            pointer++; // move onto next cmd char
+        }
+        else if (cmd[pointer] == '>')
+        {
+            if (count != 0)
+            { // check if buffer is empty
+                buffer[count] = '\0';
+                args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for buffer
+                strcpy(args[argCount], buffer);        // copy over content
+                argCount++;
+                memset(buffer, '\0', sizeof(buffer)); // clear buffer
+            }
+            args[argCount] = malloc(MAX_ARG_SIZE); // allocate memory for >
+            args[argCount] = ">";                  // add ">" to array
+            argCount++;
+            count = 0;
+        }
+        else if (!isspace(cmd[pointer]))
+        {
+            buffer[count] = cmd[pointer];
+            count++;
+        }
+        pointer++;
     }
-    args = realloc(args, count * MAX_BYTES);
-    args[count + 1] = NULL;
+    /*WRITE CODE THAT WILL HANDLE ADDING THE LAST ARG AND NULL*/
+    args[argCount] = malloc(MAX_ARG_SIZE);
+    strcpy(args[argCount], buffer);
+    argCount++;
+    args = realloc(args, (argCount + 1) * sizeof(char *));
+    args[argCount] = NULL;
     return args;
 }
 
@@ -36,7 +120,7 @@ void redirect(char **args)
 
     for (int i = 0; i < count; i++)
     { // Find the first redirect symbol (Assuming there is only one redirect)
-        if (strcmp(args[i], ">") == 0 && i != count-1)
+        if (strcmp(args[i], ">") == 0 && i != count - 1)
         {
             int fileDesc = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0777); // create file descriptor
             /*Add Error Checking here*/
@@ -46,7 +130,7 @@ void redirect(char **args)
             args[i + 1] = NULL;            // set "filename.txt" to null
             break;
         }
-        else if (strcmp(args[i], ">>") == 0 && i != count-1)
+        else if (strcmp(args[i], ">>") == 0 && i != count - 1)
         {
             int fileDesc = open(args[i + 1], O_WRONLY | O_APPEND | O_CREAT, 0777); // create file descriptor
             /*Add Error Checking here*/
@@ -96,13 +180,12 @@ int custom_system(char **args)
 
     pid_t pid;
     pid = fork();
-    
+
     if (pid == 0)
     {
         /*Child Process*/
         execvp(args[0], args);
-        perror("execv");
-        exit(1);
+        return 1;
     }
     else if (pid > 0)
     {
@@ -156,7 +239,7 @@ int main(void)
             break;
         }
 
-        char **args = whitespace_delimiter(cmd);
+        char **args = custom_parser(cmd);
 
         /* Save output redirection */
         int saved_out = dup(1);
@@ -165,6 +248,11 @@ int main(void)
         /* Execute command*/
         retval = custom_system(args);
         /* Restore output redirection */
+
+        if (retval == 1)
+        { // Command not Found
+            printf("Error: command not found\n");
+        }
         dup2(saved_out, 1);
         close(saved_out);
         fprintf(stdout, "+ completed '%s' [%d]\n", cmd, retval);
